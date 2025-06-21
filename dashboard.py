29f736bb57eaeafb9184ca788f62304337f21a1b
Spiraxspirax <125519198+Spiraxspirax@users.noreply.github.com>
2025-06-21 21:44:37 +1000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,22 @@
 
+# dashboard.py
+
+import plotly.graph_objects as go
+
+def create_radar(scores):
+    categories = list(scores.keys())
+    values = list(scores.values())
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatterpolar(
+        r=values + [values[0]],  # loop back
+        theta=categories + [categories[0]],
+        fill='toself',
+        name='Maturity'
+    ))
+
+    fig.update_layout(
+        polar=dict(radialaxis=dict(visible=True, range=[0, 5])),
+        showlegend=False
+    )
+    return fig
